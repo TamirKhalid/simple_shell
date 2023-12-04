056a0ea4524c102f9d5ee6a8399c28caf52f392d
Tamir_Khalid <tamir2khalid@gmail.com>
2023-12-04 12:39:55 -0800

    shell file

--- shell.h
+++ shell.h
@@ -1,16 +1,13 @@
 #ifndef SHELL_H
 #define SHELL_H
-
 /*
- *  Header file created by: Morgan
+ *  Header file created by: Morgan & Tamir
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
-
 /*
  * Simple_Shell Prototypes
  */
@@ -18,9 +15,4 @@ void display_prompt(void);
 void leo_print(const char *message);
 void read_command(char *command, size_t size);
 void execute_command(const char *command);
-
-
-
-
 #endif /* END */
-
