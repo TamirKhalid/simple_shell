6e811bfc5d622e04f72d959c566b0c9509de99ba
Tamir_Khalid <tamir2khalid@gmail.com>
2023-12-06 13:16:32 -0800

    updatedfile

--- shell_main.c
+++ shell_main.c
@@ -2,14 +2,14 @@
 
 /*
  * main: This is the main function
- * user-in: lenght of user input string
+ * user_in: lenght of user input string
  */
 
 int main(void) {
 char user_in[145];
 while (true) {
 dis_prompt();
-read_in(user_in, sizeof(user_in));
+read_in(user_in,i sizeof(user_in));
 exe_command(user_in);
 }
 return 0;
