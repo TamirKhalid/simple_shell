e0f0c0e11d8590c825ee52cbb9a3255fd5a06c6b
Tamir_Khalid <tamir2khalid@gmail.com>
2023-12-06 13:19:50 -0800

    updated files

--- exe.c
+++ exe.c
@@ -10,7 +10,9 @@ tamo("Error in the forking.\n");
 exit(EXIT_FAILURE);
 } else if (childpid == 0) {
 
-// Starting the child process
+/*
+ * Starting the child process
+ */
 
 char *argu[130]
 int argu_counter=0;
@@ -20,10 +22,13 @@ argu[argu_counter++] = token;
 token = strtok(NULL, " ");
 }
 argu[argu_counter] = NULL;
-// Null terminates the arguments array
 
-// Executing the command
-execvp(argu[0], argu);
+/*
+ * Null terminates the arguments array
+ * Executing the command
+*/
+
+ execvp(argu[0], argu);
 
 // If executing fails, print an error
 tamo("Error executing.\n");
