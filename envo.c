2bb1bc9767157bd55fc05b4aedcb96a31806cd0f
Tamir_Khalid <tamir2khalid@gmail.com>
2023-12-07 15:47:56 -0800

    updated file

--- envo.c
+++ envo.c
@@ -3,7 +3,9 @@
 /**
  * env_p: prints the current environment.
  */
-void tamstr(const char *format, const char *str);
+void tamstr(const char *format, const char *str){
+    write(STDOUT_FILENO, show_m, strlen(show_m));
+}
 void env_p() {
 extern char **environ;
 char **envo;
