aea050f518ba423597d9d8c2f398ebfb710418e8
Tamir_Khalid <tamir2khalid@gmail.com>
2023-12-07 13:21:10 -0800

    updated file

--- path_h.c
+++ path_h.c
@@ -1,11 +1,12 @@
 #include "shell.h"
 
 /**
- *  path_h - path function handling
+ *  path_h: path function handling
  *  @arg_value: argument value
  *  @user_in: user input 
  *  Return: the user input command for execute
  */
+
 
 char *path_h(char **arg_value, char *user_in)
 {
