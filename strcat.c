336e87f41f9a3a2c4394983e22554b9f3fc550cc
Morgan alaa <morganalaa78@gmail.com>
2023-12-07 16:48:23 -0800

    strings

---
+++ strcat.c
@@ -0,0 +1,26 @@
+#include <shell.h>
+/*
+ * _strcat:  attach the sr to des
+ *  @des: string destination 
+ *  @sr: string source
+ *  Return: string
+ */
+
+char *_strcat(char *des, char *sr)
+{
+int len = 0, led = 0, z;
+while (sr[len] != '\0')
+{
+len++;
+}
+while (des[led] != '\0')
+{
+led++;
+}
+for (z = 0; z < len; z++)
+{
+des[led + z] = sr[z];
+}
+des[led + len] = '\0';
+return (des);
+}
---
+++ strlen.c
@@ -0,0 +1,16 @@
+#include <shell.h>
+/*
+ * _strlen - function to determine the len of string
+ *  @str: string var
+ *  Return: length 
+ */
+
+int _strlen(char *str)
+{
+int y = 0;
+while (*(str + y) != '\0')
+{
+y++;
+}
+return (y);
+}
