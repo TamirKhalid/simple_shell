6d5caacd5fdcc93873079e5695829819c5cc2a61
Morgan alaa <morganalaa78@gmail.com>
2023-12-10 13:21:00 -0800

    update

--- shell_main.c
+++ shell_main.c
@@ -6,12 +6,15 @@
 */
 
 int main() {
+extern char **environ;
 char user_in[1024];
 while (true) {
 dis_prompt();
 read_in(user_in, sizeof(user_in));
 if (strcmp(user_in, "exit") == 0) {
 exit_h(user_in);
+}else if (strcmp(user_in, "env") == 0) {
+env_p(environ);
 }else {
 exe_command(user_in);
 }
