c74a047a7c3884161eb43aa44f42cba87d0e4690
Tamir_Khalid <tamir2khalid@gmail.com>
2023-12-13 09:45:28 -0800

    .

--- dis_prom.c
+++
@@ -1,9 +0,0 @@
-#include "shell.h"
-
-/*
- * dis_promt: Show the promt screen 
- */
-
-void dis_prompt(void) {
-tamo("tamo# ");
-}
--- tamo.c
+++
@@ -1,9 +0,0 @@
-#include "shell.h"
-
-/*
- * tamo: function to print the message
- */
-
-void tamo(const char *show_m) {
-    write(STDOUT_FILENO, show_m, strlen(show_m));
-}
