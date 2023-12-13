9b9a25beff7e0cbb52f1eb8ba6108ba9c9180137
Tamir_Khalid <tamir2khalid@gmail.com>
2023-12-13 10:52:20 -0800

    path_h file

--- path_h.c
+++ path_h.c
@@ -1,25 +1,26 @@
 #include "shell.h"
 
 /**
- *  * 
- *   */
+ * path_h: function that handle the path
+ * @rgu: Argument
+ * @cm: User command 
+ * Return: Execution Command
+ */
 
 char path_h(char **rgu, char *cm)
 {
-		char *p;
-
-			p = malloc(_strln("/bin/") + _strln(cm) + 1);
-				if (!p)
-						{
-									return (NULL);
-										}
-					_strcy(p, "/bin/");
-
-						if (cm[0] != '/' && cm[0] != '.')
-								{
-											rgu[0] = _strct(p, cm);
-													return (rgu[0]);
-														}
-							free(p);
-								return (cm);
+char *p;
+p = malloc(_strln("/bin/") + _strln(cm) + 1);
+if (!p)
+{
+return (NULL);
+}
+_strcy(p, "/bin/");
+if (cm[0] != '/' && cm[0] != '.')
+{
+rgu[0] = _strct(p, cm);
+return (rgu[0]);
+}
+free(p);
+return (cm);
 }
