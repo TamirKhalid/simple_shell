83e8e8a7e61bf278346c2d02af20273d74a2108d
Morganalaa <142584269+Morganalaa@users.noreply.github.com>
2023-12-14 20:40:35 +0200

    Update read_l.c

--- read_l.c
+++ read_l.c
@@ -3,20 +3,11 @@
  * read_l: function that read user input
  * Return: return pointer to the function or Null when fail
  */
-/* char *read_l(void) {
+char *read_l(void) {
 char *line = NULL;
   size_t bufsize = 0;
 if (getline(&line, &bufsize, stdin) == -1) {
 free(line);
 }
 return NULL;
-} */
-char *read_l(void) {
-    char *line = NULL;
-    size_t bufsize = 0;
-    if (getline(&line, &bufsize, stdin) == -1) {
-        free(line); // Free allocated memory on failure
-        return NULL; // Return NULL on failure
-    }
-    return line; // Return the read line
 }
