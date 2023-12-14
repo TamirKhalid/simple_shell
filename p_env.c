657e093217b81da83334498cecb7a0d06fe7157d
Morganalaa <142584269+Morganalaa@users.noreply.github.com>
2023-12-14 20:14:54 +0200

    Update p_env.c

--- p_env.c
+++ p_env.c
@@ -1,4 +1,21 @@
 #include "shell.h"
+
+/*
+ *  * _strlen - function to determine the len of string
+ *   *  @str: string var
+ *    *  Return: length 
+ **/
+
+int _strlen(char *str)
+{
+	int y = 0;
+	while (*(str + y) != '\0')
+	{
+		y++;
+	}
+	return (y);
+}
+
 /*
  * p_env: function to print enviroment variable
  */
