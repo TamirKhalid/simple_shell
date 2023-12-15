23e0df2d5fbc90070f7f083f6143d570fe66c9da
Tamir_Khalid <tamir2khalid@gmail.com>
2023-12-15 15:17:44 -0800

    updated file

--- list_f.c
+++ list_f.c
@@ -88,4 +88,50 @@ node = node->next;
 }
 return (0);
 }
-
+/**
+ * list_len - Function that determine length of list
+ * @h: First Node Pointer
+ * Return: Size
+ */
+size_t list_len(const list_t *h)
+{
+size_t i = 0;
+while (h)
+{
+h = h->next;
+i++;
+}
+return (i);
+}
+/**
+ * list_to_strings - Function that return array of strings
+ * @head: First Node pointer
+ * Return: Array
+ */
+char **list_to_strings(list_t *head)
+{
+list_t *node = head;
+size_t i = list_len(head), j;
+char **strs;
+char *str;
+if (!head || !i)
+return (NULL);
+strs = malloc(sizeof(char *) * (i + 1));
+if (!strs)
+return (NULL);
+for (i = 0; node; node = node->next, i++)
+{
+str = malloc(_strlen(node->str) + 1);
+if (!str)
+{
+for (j = 0; j < i; j++)
+free(strs[j]);
+free(strs);
+return (NULL);
+}
+str = _strcpy(str, node->str);
+strs[i] = str;
+}
+strs[i] = NULL;
+return (strs);
+}
