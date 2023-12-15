6728df7feba1edce42f2785a644d7f28f7ccbaca
Morgan alaa <morganalaa78@gmail.com>
2023-12-14 17:11:09 -0800

    list_f.c

---
+++ list_f.c
@@ -0,0 +1,91 @@
+#include "shell.h"
+
+/**
+ *  add_node - adds node at start of list
+ *  @head: address of head node
+ *  @str: string of node
+ *  @num: node indexx
+ *  Return: size 
+ */
+
+list_t *add_node(list_t **head, const char *str, int num)
+{
+list_t *new_head;
+if (!head)
+return (NULL);
+new_head = malloc(sizeof(list_t));
+if (!new_head)
+return (NULL);
+_memset((void *)new_head, 0, sizeof(list_t));
+new_head->num = num;
+if (str)
+{
+new_head->str = _strdup(str);
+if (!new_head->str)
+{
+free(new_head);
+return (NULL);
+}
+}
+new_head->next = *head;
+*head = new_head;
+return (new_head);
+}
+
+/**
+ *  print_ls - prints the string of a list_t
+ *  @h:  first node pointer 
+ *  Return: size
+ */
+
+size_t print_ls(const list_t *h)
+{
+size_t x = 0;
+while (h)
+{
+_puts(h->str ? h->str : "(nil)");
+_puts("\n");
+h = h->next;
+x++;
+}
+return (x);
+}
+
+/**
+ *  delete_node_at_index - deletes node
+ *  @head: address of first node
+ *  @index: index to delete
+ *  Return: 1  success, 0  failure
+ */
+
+int delete_node_at_index(list_t **head, unsigned int index)
+{
+list_t *node, *prev_node;
+unsigned int i = 0;
+if (!head || !*head)
+return (0);
+if (!index)
+{
+node = *head;
+*head = (*head)->next;
+free(node->str);
+free(node);
+return (1);
+}
+node = *head;
+while (node)
+{
+if (i == index)
+{
+prev_node->next = node->next;
+free(node->str);
+free(node);
+return (1);
+}
+i++;
+prev_node = node;
+node = node->next;
+}
+return (0);
+}
+
