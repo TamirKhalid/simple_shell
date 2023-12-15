70921dd485b36ff3e0f6f399fea2000e33fb22bd
Tamir_Khalid <tamir2khalid@gmail.com>
2023-12-15 15:29:18 -0800

    updated file

--- env_f.c
+++ env_f.c
@@ -61,3 +61,40 @@ for (x = 1; x <= stino->argc; x++)
 _unsetenv(stino, stino->argv[x]);
 return (0);
 }
+/**
+ * add_node_end - Function that adds  node to the list
+ * @head: head Note Address Pointer
+ * @str: field of node strings
+ * @num: History index
+ * Return: Size
+ */
+list_t *add_node_end(list_t **head, const char *str, int num)
+{
+list_t *new_node, *node;
+if (!head)
+return (NULL);
+node = *head;
+new_node = malloc(sizeof(list_t));
+if (!new_node)
+return (NULL);
+_memset((void *)new_node, 0, sizeof(list_t));
+new_node->num = num;
+if (str)
+{
+new_node->str = _strdup(str);
+if (!new_node->str)
+{
+free(new_node);
+return (NULL);
+}
+}
+if (node)
+{
+while (node->next)
+node = node->next;
+node->next = new_node;
+}
+else
+*head = new_node;
+return (new_node);
+}
