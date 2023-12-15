0371a67534a65310dedf4f15bfc2285317299c2c
Morgan alaa <morganalaa78@gmail.com>
2023-12-15 14:52:59 -0800

    update

--- ex_functions.c
+++ ex_functions.c
@@ -1,38 +1,5 @@
 #include "shell.h"
 
-/*
-* _puts - Function that print input string 
-*/
-void _puts(char *str)
-{
-int x = 0;
-if (!str)
-return;
-while (str[x] != '\0')
-{
-_putchar(str[x]);
-x++;
-}
-}
-/**
- * _putchar - Function that writes character to stdout
- * @c: Printable character
- * Return: Sucess 1, Error -1, 
- */
-int _putchar(char c)
-{
-static int i;
-static char buf[WRITE_BUF_SIZE];
-if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-{
-write(1, buf, i);
-i = 0;
-}
-if (c != BUF_FLUSH)
-buf[i++] = c;
-return (1);
-}
-
 /**
  * starts_with - Function that checks if needle starts with haystack
  * @haystack: search Sting 
