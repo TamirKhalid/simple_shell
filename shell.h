30f9a09bba4ee6c3e7214107700200675918751a
Morgan alaa <morganalaa78@gmail.com>
2023-12-15 13:07:06 -0800

    update s

--- shell.h
+++ shell.h
@@ -75,8 +75,7 @@ int readfd;
 } info_t;
 
 #define INFO_INIT \
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
-		0, 0, 0}
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \  0, 0, 0}
 
 /**
  * struct builtin - contains a builtin string and related function
