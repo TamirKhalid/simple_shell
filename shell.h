2f8da05441b14de22de84d94142383cfb3332a79
Morgan alaa <morganalaa78@gmail.com>
2023-12-15 12:52:58 -0800

    update s

--- shell.h
+++ shell.h
@@ -17,7 +17,11 @@
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
+
+
+
 extern char **environ;
+
 
 /**
  * struct liststr - singly linked list
@@ -25,119 +29,130 @@ extern char **environ;
  * @str: a string
  * @next: points to the next node
  */
+
 typedef struct liststr
 {
-	int num;
-	char *str;
-	struct liststr *next;
+int num;
+char *str;
+struct liststr *next;
 } list_t;
 
 /**
- *struct passinfo - pseudo-arguements for function
- *@arg: a string generated from getline containing arguements
- *@argv: an array of strings generated from arg
- *@path: a string path for the current command
- *@argc: the argument count
- *@line_count: the error count
- *@err_num: the error code for exit()s
- *@linecount_flag: if on count this line of input
- *@env: linked list local copy of environ
- *@environ: custom modified copy of environ from LL env
- *@env_changed: on if environ was changed
- *@status: the return status of the last exec'd command
- *@cmd_buf: address of pointer to cmd_buf, on if chaining
- *@cmd_buf_type: CMD_type ||, &&, ;
- *@readfd: the fd from which to read line input
- *@fname: the program filename
- */
+ *struct passinfo - contains pseudo-arguements to pass into a function,
+ *allowing uniform prototype for function pointer struct
+ * @arg: a string generated from getline containing arguements
+ * @argv: an array of strings generated from arg
+ * @path: a string path for the current command
+ * @argc: the argument count
+ * @line_count: the error count
+ * @err_num: the error code for exit()s
+ * @linecount_flag: if on count this line of input
+ * @fname: the program filename
+ * @env: linked list local copy of environ
+ * @environ: custom modified copy of environ from LL env
+ * @env_changed: on if environ was changed
+ * @status: the return status of the last exec'd command
+ * @cmd_buf: address of pointer to cmd_buf, on if chaining
+ * @cmd_buf_type: CMD_type ||, &&, ;
+ * @readfd: the fd from which to read line input
+ **/
+
 typedef struct passinfo
 {
-	char *arg;
-	char **argv;
-	char *path;
-	int argc;
-	unsigned int line_count;
-	int err_num;
-	int linecount_flag;
-	list_t *env;
-	char **environ;
-	int env_changed;
-	int status;
-	char *fname;
-
-	char **cmd_buf; 
-	int cmd_buf_type;
-	int readfd;
+char *arg;
+char **argv;
+char *path;
+int argc;
+unsigned int line_count;
+int err_num;
+int linecount_flag;
+char *fname;
+list_t *env;
+char **environ;
+int env_changed;													int status;
+char **cmd_buf;	
+int readfd;
 } info_t;
 
 #define INFO_INIT \
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
-	0, 0, 0}
+		0, 0, 0}
 
 /**
- *struct builtin - contains a builtin string and related function
- *@type: the builtin command flag
- *@func: the function
- */
+ * struct builtin - contains a builtin string and related function
+ * @type: the builtin command flag
+ * @func: the function
+ **/
+
 typedef struct builtin
 {
-	char *type;
-	int (*func)(info_t *);
+char *type;
+int (*func)(info_t *);
 } builtin_table;
 
 
-/* main_l.c */
+
 int main_l(info_t *, char **);
 int find_builtin(info_t *);
 void srch_cmd(info_t *);
 void fork_cmd(info_t *);
 
-/* path.c */
 int is_exe(info_t *, char *);
 char *ex_col(char *, int, int);
 char *srch_path(info_t *, char *, char *);
 
-/* write_f.c */
+int loophsh(char **);
 void _mputs(char *);
 int _mputchar(char);
+
+int _strlen(char *);
+int _strcmp(char *, char *);
+char *starts_with(const char *, const char *);
+char *_strcat(char *, char *);
+
+char *_strcpy(char *, char *);
+char *_strdup(const char *);
 void _puts(char *);
 int _putchar(char);
 
-/* string_f.c */
-int _strlen(char *);
-int _strcmp(char *, char *);
-char *_strcat(char *, char *);
-char *_strcpy(char *, char *);
+char *_memset(char *, char, unsigned int);
+int interactive(info_t *);
+int is_delim(char, char *);
+int _isalpha(int);
+int _atoi(char *);
 
-/* env_f.c */
-int env_p(info_t *)
-int _mysetenv(info_t *)
-int _myunsetenv(info_t *)
+int _errti(char *);
+void error_p(info_t *, char *);
+int print_d(int, int);
 
-/* env_f2.c */
-char **ret_environ(info_t *
-int _unsetenv(info_t *, char *)
-int _setenv(info_t *, char *, char *)
+int exit_m(info_t *);
 
-/* ex_functions.c */
-size_t print_ls(const list_t *)
-void _puts(char *)
-int _putchar(char )
-char *starts_with(const char *, const char *)
-int is_delim(char c, char *)
+ssize_t get_input(info_t *);
+int _getline(info_t *, char **, size_t *);
 
-/* ex_functions2.c */
-char *_memset(char *, char , unsigned int )
-int _errti(char *)
-void error_p(info_t *, char *)
-int print_d(int , int )
+void clear_info(info_t *);
+void set_info(info_t *, char **);
+void free_info(info_t *, int);
+char *_getenv(info_t *, const char *);
+int env_p(info_t *);
+int _mysetenv(info_t *);
+int _myunsetenv(info_t *);
+int pop_env_list(info_t *);
 
-/* exit_f.c */
-int exit_m(info_t *)
+char **ret_environ(info_t *);
+int _unsetenv(info_t *, char *);
+int _setenv(info_t *, char *, char *);
 
-/* list_f.c */
-list_t *add_node(list_t **, const char *, int )
-size_t print_ls(const list_t *)
-int delete_node_at_index(list_t **, unsigned int )
+list_t *add_node(list_t **, const char *, int);
+list_t *add_node_end(list_t **, const char *, int);
+size_t print_ls(const list_t *);
+int delete_node_at_index(list_t **, unsigned int);
+void free_list(list_t **);
+size_t list_len(const list_t *);
+char **list_to_strings(list_t *);
+size_t print_list(const list_t *);
+list_t *node_starts_with(list_t *, char *, char);
+ssize_t get_node_index(list_t *, list_t *);
+
 
 #endif
--- write_f.c
+++ write_f.c
@@ -1,9 +1,11 @@
 #include "shell.h"
+
 /**
  *_mputs - Function that prints input string
  * @str: printed string 
  * Return: nothingg
  */
+
 void _mputs(char *str)
 {
 int x = 0;
@@ -15,11 +17,13 @@ _mputchar(str[x]);
 x++;
 }
 }
+
 /**
  * _mputchar - Function that writes character 
  * @c: Printed character 
  * Return: success 1, error -1, 
  */
+
 int _mputchar(char c)
 {
 static int x;
@@ -33,11 +37,13 @@ if (c != BUF_FLUSH)
 buf[x++] = c;
 return (1);
 }
+
 /**
  *_puts - Function that print input string
  *@str: Printed string 
  * Return: Null
  */
+
 void _puts(char *str)
 {
 int i = 0;
@@ -49,11 +55,13 @@ _putchar(str[i]);
 i++;
 }
 }
+
 /**
  * _putchar - Function that writes character to stdout
  * @c: Printed Char
  * Return: success 1, error -1 
  */
+
 int _putchar(char c)
 {
 static int i;
